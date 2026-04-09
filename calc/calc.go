713bf08fba5bf883c3cdce2d162a5729e933aa5c
pptfz <pptfzo@gmail.com>
2026-04-09 15:48:25 +0800

    update calc.go

---
+++ calc/calc.go
@@ -0,0 +1,11 @@
+package calc
+
+func Add(a int, b int) int {
+	result := a
+
+	for i := 0; i < b; i++ {
+		result = result + 1
+	}
+
+	return result
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+        "fmt"
+        "git-ai-demo/calc"
+)
+
+func main() {
+        result := calc.Add(1, 2)
+        fmt.Println("result:", result)
+}
